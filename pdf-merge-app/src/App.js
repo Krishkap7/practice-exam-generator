b57fb460b0caef18fd9824399713f45cc170384e
Aryaman Goenka <arygoenka@gmail.com>
2023-11-18 19:38:50 -0500

    front UI works better

--- pdf-merge-app/src/App.js
+++ pdf-merge-app/src/App.js
@@ -1,13 +1,46 @@
-import React from 'react';
-import FileUpload from './FileUpload';
+import React, { useRef, useState, useCallback } from 'react';
+import { useDropzone } from 'react-dropzone';
+import './App.css'; // Add this line at the top of your file
 
-function App() {
+
+export default function App() {
+  const finalizeButtonRef = useRef(null);
+  const [finalizeButtonLoading, setFinalizeButtonLoading] = useState(false);
+  const [finalizeButtonDisabled, setFinalizeButtonDisabled] = useState(false);
+  const [files, setFiles] = useState([]);
+
+  const onDrop = useCallback(acceptedFiles => {
+    // Append new files to the existing files
+    setFiles(prevFiles => [...prevFiles, ...acceptedFiles]);
+  }, []);
+
+  const { getRootProps, getInputProps } = useDropzone({
+    onDrop,
+    accept: 'application/pdf',
+    multiple: true
+  });
+
+  // Rest of your component logic...
+
   return (
-    <div className="App">
-      <h1>PDF Merge Tool</h1>
-      <FileUpload />
-    </div>
+    <>
+      <div {...getRootProps()} className="dropzone">
+        <input {...getInputProps()} />
+        <p>Drag and drop PDF files here, or click to select files</p>
+      </div>
+      {files.length > 0 && (
+        <ul className="file-list">
+          {files.map(file => (
+            <li key={file.path}>{file.name} - {file.size} bytes</li>
+          ))}
+        </ul>
+      )}
+      <button
+        ref={finalizeButtonRef}
+        disabled={finalizeButtonLoading || finalizeButtonDisabled}
+      >
+        {finalizeButtonLoading ? 'Loading...' : 'Finalize'}
+      </button>
+    </>
   );
 }
-
-export default App;
